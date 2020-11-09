c5ed671f17304f946a114c14e9eceae532800246
Carlos Lomeli <sackrone@hotmail.com>
2020-11-09 13:40:14 -0600

    Parte 07

---
+++ CursoMVC/Controllers/CloseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class CloseController : Controller
+    {
+        public ActionResult LogOff()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Access");
+        }
+    }
+}
