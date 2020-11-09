8ff34884e28a5a67dc032f6c4612fd74e4276928
Carlos Lomeli <sackrone@hotmail.com>
2020-11-09 13:30:55 -0600

    Parte 06

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -1,7 +1,8 @@
 ﻿using CursoMVC.Models;
 using CursoMVC.Models.TableViewModels;
-using System.Linq;
+using CursoMVC.Models.ViewModels;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace CursoMVC.Controllers
@@ -15,17 +16,104 @@ namespace CursoMVC.Controllers
             using (cursomvcEntities db = new cursomvcEntities())
             {
                 lst = (from d in db.user
-                      where d.idState == 1
-                      orderby d.email
-                      select new UserTableViewModel
-                      {
-                          Email = d.email,
-                          Id = d.id,
-                          Edad = d.edad
-                      }).ToList();
+                       where d.idState == 1
+                       orderby d.id
+                       select new UserTableViewModel
+                       {
+                           Email = d.email,
+                           Id = d.id,
+                           Edad = d.edad
+                       }).ToList();
             }
 
             return View(lst);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (cursomvcEntities db = new cursomvcEntities())
+            {
+                user oUser = new user
+                {
+                    idState = 1,
+                    email = model.Email,
+                    edad = model.Edad,
+                    password = model.Password
+                };
+
+                db.user.Add(oUser);
+                db.SaveChanges();
+            }
+
+            return Redirect(Url.Content("~/User/"));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            EditUserViewModel model = new EditUserViewModel();
+            using (cursomvcEntities db = new cursomvcEntities())
+            {
+                user oUser = db.user.Find(Id);
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (cursomvcEntities db = new cursomvcEntities())
+            {
+                user oUser = db.user.Find(model.Id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                if (model.Password != null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Redirect(Url.Content("~/User/"));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            using (cursomvcEntities db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                oUser.idState = 2; //Eliminar
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Content("1");
         }
     }
 }
---
+++ CursoMVC/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El campo {0} debe tener al menos {1} caractéres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no son iguales")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El campo {0} debe tener al menos {1} caractéres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no son iguales")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
